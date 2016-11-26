08db682d90481b565b1f4813594f868e2c1bfb41
Darcio Gomes <darcio@darcweb.com.br>
2016-11-26 13:31:15 -0300

    wrgeg

--- setup.php
+++ setup.php
@@ -10,11 +10,13 @@ $params['sysco']['errors'] = true;
 $params['sysco']['syskey'] = "JHJIHSJH68HJ9870985JHG765BKHJLLS";
 
 //configurações de layout
-$params['sysco']['title'] = "Sysco - v 4.0";
+$params['sysco']['title'] = "Sysco ffgf - v 4.0";
 $params['sysco']['author'] = "Darc Web";
 $params['sysco']['copyright'] = "Copyright © 2016 - {author} - Todos os direitos reservados";
 $params['sysco']['baseurl'] = $_SERVER['SERVER_NAME'];
 $params['sysco']['url'] = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+
+
 
 //configurações de base
 $params['sysco']['system'] = $_SERVER['SYSTEM'];
